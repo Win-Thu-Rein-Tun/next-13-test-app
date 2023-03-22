f4c826a4fcdf643b747631c9cae9933803a1b85e
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-22 18:02:15 +0630

    okay login

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -5,14 +5,14 @@ import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
 export default async function Nav() {
   const session = await getServerSession(authOptions);
-  console.log(session);
   return (
     <nav className="flex justify-between items-center py-8">
       <Link href={"/"}>
         <h1 className="font-bold text-lg">Sent it</h1>
       </Link>
       <ul className="flex items-center gap-6">
-        <Login />
+        {!session?.user && <Login />}
+        {session?.user && <h1>{session.user.name}</h1>}
       </ul>
     </nav>
   );
