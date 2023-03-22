ffe2e9784a554e294feb27225967ffd16966dc4f
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-22 18:31:32 +0630

    g image okay

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -5,7 +5,10 @@ import { signIn } from "next-auth/react";
 export default function Login() {
   return (
     <li className="list-none">
-      <button onClick={() => signIn()} className="text-sm bg-gray-700 text-white rounded-xl py-2 px-6 disabled:opacity-25">
+      <button
+        onClick={() => signIn()}
+        className="text-sm bg-gray-700 text-white rounded-xl py-2 px-6 disabled:opacity-25"
+      >
         Sign In
       </button>
     </li>
