3cbd81c95cc23afd551ab41c6c39e3ff41d422fc
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-22 23:51:49 +0630

    fix use client error

--- app/auth/QueryWrapper.tsx
+++ app/auth/QueryWrapper.tsx
@@ -1,14 +1,16 @@
+"use client"
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 
 interface Props {
-    children?: ReactNode
+  children?: ReactNode;
 }
 
-const queryClient = new QueryClient
+const queryClient = new QueryClient();
 
-const QueryWrapper = ({children}: Props) => (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-)
+const QueryWrapper = ({ children }: Props) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
 
-export default QueryWrapper
+export default QueryWrapper;
