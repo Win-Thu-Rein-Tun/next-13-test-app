8f1201653d4373b32967cfa7175cf3ef9152f5da
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-24 21:54:32 +0630

    getpost api

---
+++ pages/api/posts/getPost.tsx
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    //Get
+    try {
+      const post = await prisma.post.findMany();
+      res.status(200).json(post);
+    } catch (error) {
+      res.status(403).json({ err: "Error while getting posts" });
+    }
+  }
+}
