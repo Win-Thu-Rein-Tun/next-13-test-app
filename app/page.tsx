291ef11b68f81a4f87a75721e37e8232f42c3206
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-24 22:03:35 +0630

    fetch post useQuery

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-"use client"
+"use client";
 
 import CreatePost from "./components/CreatePost";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
-const allPosts = async() => {
-  const response = await axios.get("/api/posts/getPosts")
-  return response.data
-}
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
   return (
     <main>
       <div>Hello Next</div>
-      <CreatePost/>
+      <CreatePost />
     </main>
   );
 }
