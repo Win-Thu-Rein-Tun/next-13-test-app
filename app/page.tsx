e2dbd4ce7866d09ff28777c38c7b4e3293439f9e
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-24 22:00:13 +0630

    fetch post

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 "use client"
 
 import CreatePost from "./components/CreatePost";
+import axios from "axios";
 
+const allPosts = async() => {
+  const response = await axios.get("/api/posts/getPosts")
+  return response.data
+}
 export default function Home() {
   return (
     <main>
