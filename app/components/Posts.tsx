4d258831d349d347d174ea3c75e37f05096878c9
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-25 23:14:05 +0630

    before fetch

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -3,5 +3,13 @@ import Link from "next/link"
 import Image from "next/image"
 
 const Posts = () => (
-    <div></div>
+    <div>
+        <div>
+            <Image width={32} height={32} src={avatar} alt="avatar"/>
+            <h3>{name}</h3>
+        </div>
+        <div>
+            <p>{postTitle}</p>
+        </div>
+    </div>
 )
