222256b849fbb7db591da2be554997317c60e9b2
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-25 23:32:18 +0630

    style posts

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -3,13 +3,13 @@ import Link from "next/link"
 import Image from "next/image"
 
 const Posts = ({name, avatar, postTitle}) => (
-    <div className="my-8 p-8 bg-white">
+    <div className="my-8 p-8 bg-white rounded-lg">
         <div className="flex items-center gap-2">
             <Image width={32} height={32} src={avatar} alt="avatar" className="rounded-full"/>
-            <h3>{name}</h3>
+            <h3 className="fonts-bold text-gray-700">{name}</h3>
         </div>
-        <div>
-            <p>{postTitle}</p>
+        <div className="my-8">
+            <p className="break-all">{postTitle}</p>
         </div>
     </div>
 )
