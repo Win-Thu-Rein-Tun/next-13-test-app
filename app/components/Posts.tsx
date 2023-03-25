37ea67101cae1ad66fd0db2c80b5ba884626a93b
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-25 23:43:18 +0630

    add comments

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,17 +1,26 @@
-"use client"
-import Link from "next/link"
-import Image from "next/image"
+"use client";
+import Link from "next/link";
+import Image from "next/image";
 
-const Posts = ({name, avatar, postTitle}) => (
-    <div className="my-8 p-8 bg-white rounded-lg">
-        <div className="flex items-center gap-2">
-            <Image width={32} height={32} src={avatar} alt="avatar" className="rounded-full"/>
-            <h3 className="fonts-bold text-gray-700">{name}</h3>
-        </div>
-        <div className="my-8">
-            <p className="break-all">{postTitle}</p>
-        </div>
+const Posts = ({ name, avatar, postTitle, id }) => (
+  <div className="my-8 p-8 bg-white rounded-lg">
+    <div className="flex items-center gap-2">
+      <Image
+        width={32}
+        height={32}
+        src={avatar}
+        alt="avatar"
+        className="rounded-full"
+      />
+      <h3 className="font-bold text-gray-700">{name}</h3>
     </div>
-)
+    <div className="my-8">
+      <p className="break-all">{postTitle}</p>
+    </div>
+    <div className="flex gap-4 items-center cursor-pointer">
+      <Link href={`/post/${id}`}><p className="text-sm font-bold text-gray-700">Comments</p></Link>
+    </div>
+  </div>
+);
 
-export default Posts
+export default Posts;
