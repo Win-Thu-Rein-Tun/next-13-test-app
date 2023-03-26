4dc0ab55f5eda39fa9b82a9da32807eb408bd1f4
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-26 20:06:34 +0630

    change in client.tsx

--- prisma/client.tsx
+++ prisma/client.tsx
@@ -1,6 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = (globalThis as any).prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") (globalThis as any).prisma = client;
+declare global {
+  namespace NodeJS {
+    interface Global {}
+  }
+}
+
+interface CustomNodeJsGlobal extends NodeJS.Global {
+    prisma: PrismaClient
+}
+
+declare const global: CustomNodeJsGlobal
+
+const client = global.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") global.prisma = client;
 
 export default client;
