3aae1dd6b30f3c5da5ee499233792b7720c9e579
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-27 10:14:20 +0630

    some code in authpost.ts

---
+++ app/types/AuthPosts.ts
@@ -0,0 +1,18 @@
+export type AuthPost = {
+  email: string;
+  id: string;
+  image: string;
+  name: string;
+  Post: {
+    createdAt: string;
+    id: string;
+    title: string;
+  };
+  Comment?: {
+    createdAt: string;
+    id: string;
+    title: string;
+    postId: string;
+    userId: string;
+  }[];
+};
