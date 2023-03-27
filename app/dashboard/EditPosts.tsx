5ee03157be9772805f88949cf97d7cb024db5e42
Sun-D-Shine <winthureintun2000@gmail.com>
2023-03-27 11:54:10 +0630

    minor fix

--- app/dashboard/EditPosts.tsx
+++ app/dashboard/EditPosts.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import { useState } from "react"
-// import Toggle from "./Toggle"
-// import { useMutation, useQueryClient } from "react-query"
-// import toast from "react-hot-toast"
+import Image from "next/image";
+import { useState } from "react";
+import Toggle from "./Toggle";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 // import { motion } from "framer-motion"
-import axios from "axios"
+import axios from "axios";
 
 type EditProps = {
-  id: string
-  avatar: string
-  name: string
-  title: string
+  id: string;
+  avatar: string;
+  name: string;
+  title: string;
   comments?: {
-    id: string
-    postId: string
-    userId: string
-  }[]
-}
+    id: string;
+    postId: string;
+    userId: string;
+  }[];
+};
 
 export default function EditPost({
   avatar,
@@ -27,61 +27,67 @@ export default function EditPost({
   comments,
   id,
 }: EditProps) {
-//   const [toggle, setToggle] = useState(false)
-//   const queryClient = useQueryClient()
-//   let deleteToastID: string
+  const [toggle, setToggle] = useState(false);
+  const queryClient = useQueryClient();
+  let deleteToastID: string;
 
-//   const { mutate } = useMutation(
-//     async (id: string) =>
-//       await axios.delete("/api/posts/deletePost", { data: id }),
-//     {
-//       onError: (error) => {
-//         console.log(error)
-//       },
-//       onSuccess: (data) => {
-//         console.log(data)
-//         queryClient.invalidateQueries("getAuthPosts")
-//         toast.success("Post has been deleted.", { id: deleteToastID })
-//       },
-//     }
-//   )
+  const { mutate } = useMutation(
+    async (id: string) =>
+      await axios.delete("/api/posts/deletePost", { data: id }),
+    {
+      onError: (error) => {
+        console.log(error);
+      },
+      onSuccess: (data) => {
+        console.log(data);
+        queryClient.invalidateQueries(["auth-posts"]);
+        toast.success("Post has been deleted.", { id: deleteToastID });
+      },
+    }
+  );
 
-//   const deletePost = () => {
-//     deleteToastID = toast.loading("Deleting your post.", { id: deleteToastID })
-//     mutate(id)
-//   }
+  const deletePost = () => {
+    deleteToastID = toast.loading("Deleting your post.", { id: deleteToastID });
+    mutate(id);
+  };
 
   return (
     <>
-      {/* <motion.div
-        animate={{ opacity: 1, scale: 1 }}
-        initial={{ opacity: 0, scale: 0.8 }}
-        transition={{ ease: "easeOut" }}
+      <div
+        // animate={{ opacity: 1, scale: 1 }}
+        // initial={{ opacity: 0, scale: 0.8 }}
+        // transition={{ ease: "easeOut" }}
         className="bg-white my-8 p-8 rounded-lg "
-      > */}
+      >
         <div className="flex items-center gap-2">
-          <Image width={32} height={32} src={avatar} alt="avatar" />
+          <Image
+            width={32}
+            height={32}
+            src={avatar}
+            alt="avatar"
+            className="rounded-full"
+          />
           <h3 className="font-bold text-gray-700">{name}</h3>
         </div>
         <div className="my-8 ">
-          <p className="break-all">{title}</p>
+          <p className="break-all text-sm text-gray-700">{title}</p>
         </div>
-        {/* <div className="flex items-center gap-4 ">
+        <div className="flex items-center gap-4 ">
           <p className=" text-sm font-bold text-gray-700">
             {comments?.length} Comments
           </p>
           <button
             onClick={(e) => {
-              e.stopPropagation()
-              setToggle(true)
+              e.stopPropagation();
+              setToggle(true);
             }}
             className="text-sm font-bold text-red-500"
           >
             Delete
           </button>
-        </div> */}
-      {/* </motion.div>
-      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />} */}
+        </div>
+      </div>
+      {toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
     </>
-  )
+  );
 }
